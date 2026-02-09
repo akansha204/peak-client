33cc9f1047d8158d079dbfed681042977783f5e9
Akansha Roy <akansharoy2008@gmail.com>
2026-02-09 21:05:17 +0530

    initial commit

---
+++ src/main/java/com/akansha/peak_client/PeakClientApplication.java
@@ -0,0 +1,13 @@
+package com.akansha.peak_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akansha/peak_client/PeakClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akansha.peak_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
